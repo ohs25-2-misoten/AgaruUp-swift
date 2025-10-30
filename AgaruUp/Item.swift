0b8459e55c653dc99ee72835e413719791480ca9
alpha9n <42644369+Alpha9n@users.noreply.github.com>
2025-10-30 12:05:48 +0900

    Initial Commit

---
+++ AgaruUp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AgaruUp
+//
+//  Created by 神保恒介 on 2025/10/30.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AgaruUpTests/AgaruUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  AgaruUpTests.swift
+//  AgaruUpTests
+//
+//  Created by 神保恒介 on 2025/10/30.
+//
+
+import Testing
+@testable import AgaruUp
+
+struct AgaruUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
