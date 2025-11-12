b811c081e5ff6ad522a70543dc19c1290dab3e86
takumiino <hal.bandotakumi@gmail.com>
2025-11-12 11:25:26 +0900

    AppBarコンポーネントの作成

---
+++ AgaruUp/Views/Components/AppBar/AppBar.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct SearchAppBarButtonModifier: ViewModifier {
+
+  let action: () -> Void
+
+  func body(content: Content) -> some View {
+    content
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          Button(action: action) {
+            Image(systemName: "magnifyingglass")
+          }
+        }
+      }
+  }
+}
+
+extension View {
+  func addSearchAppBarButton(action: @escaping () -> Void) -> some View {
+    self.modifier(SearchAppBarButtonModifier(action: action))
+  }
+}
+
+#Preview {
+  NavigationStack {
+    Text("コンテンツ表示エリア")
+      .addSearchAppBarButton {
+        print("Search button tapped!")
+      }
+  }
+}
